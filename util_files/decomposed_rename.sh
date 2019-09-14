c357154019fc221c7ce0e34262a956d3f1e940e4
Lakshamana <guitrompa1@gmail.com>
2019-09-14 18:47:22 -0300

    fix path files renaming

--- util_files/decomposed_rename.sh
+++ util_files/decomposed_rename.sh
@@ -2,7 +2,7 @@
 
 for file in `find . -regextype posix-extended -regex '.*[Dd]ecomposed[_]*[Aa]ctivity.*'`; do
   echo $file
-  replace=`echo $file | sed -r 's/(.*)([Dd]ecomposed)[_]*[Aa]ctivity(.*)/\2\3/'`
+  replace=`echo $file | sed -r 's/(.*)([Dd]ecomposed)[_]*[Aa]ctivity(.*)/\1\2\3/'`
   mv $file $replace
 done
 
