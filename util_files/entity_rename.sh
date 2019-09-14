f5b3db1413283900f7529bb00036bb9c08f5da0b
Lakshamana <guitrompa1@gmail.com>
2019-09-14 20:44:14 -0300

    Rename PlainAcitivity to Plain

--- util_files/entity_rename.sh
+++ util_files/entity_rename.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in `find . -regextype posix-extended -regex '.*[Dd]ecomposed[_]*[Aa]ctivity.*'`; do
+for file in `find . -regextype posix-extended -regex '.*[Pp]lain[_]*[Aa]ctivity.*'`; do
   echo $file
-  replace=`echo $file | sed -r 's/(.*)([Dd]ecomposed)[_]*[Aa]ctivity(.*)/\1\2\3/'`
+  replace=`echo $file | sed -r 's/(.*)([Pp]lain)[_]*[Aa]ctivity(.*)/\1\2\3/'`
   mv $file $replace
 done
 
