f4be74feebfeef7d58ca79bbc76de74e984f8ea1
Lakshamana <guitrompa1@gmail.com>
2019-09-15 19:01:25 -0300

    fix Message package declaration

--- src/main/java/br/ufpa/labes/spm/repository/interfaces/plannerInfo/IPeopleInstantiationSuggestionDAO.java
+++ src/main/java/br/ufpa/labes/spm/repository/interfaces/plannerInfo/IPeopleInstantiationSuggestionDAO.java
@@ -1,7 +1,5 @@
 package br.ufpa.labes.spm.repository.interfaces.plannerInfo;
 
-
-
 import br.ufpa.labes.spm.repository.interfaces.IBaseDAO;
 import br.ufpa.labes.spm.domain.PeopleInstantiationSuggestion;
 
