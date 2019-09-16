eef59a304ea92973e443b5e05e818dda3fc4fade
Lakshamana <guitrompa1@gmail.com>
2019-09-16 10:56:26 -0300

    fix subclasses get list

--- util_files/inheritance.sh
+++ util_files/inheritance.sh
@@ -3,8 +3,6 @@ utildir="`readlink -m .`/util_files"
 cd $HOME/Documentos/spm_root/SPMServices/ejbModule/org/qrconsult/spm
 classes=`grep -iRl '@Inheritance(strategy=InheritanceType.JOINED)' | sed '/policies/d' | cut -d'/' -f3 | sed -r 's/(.*).java/\1/'`
 
-echo "current: $currentdir"
-
 cd $HOME/git/spm/
 model_path='src/main/java/br/ufpa/labes/spm/domain'
 cd $model_path
@@ -28,9 +26,9 @@ for c in $classes; do
   sed -ri "/package/ s/(package .*)/\1\n\nimport javax.persistence.Inheritance;\nimport javax.persistence.InheritanceType;\n/" $class_path
 
   # Add 'extends' to subclasses
-  subclasses=`cat "$utildir/subclasses.txt" | grep "$c" | awk '{print $2}'`
+  subclasses=`cat "$utildir/subclasses.txt" | grep -w "$c" | awk '{print $2}' | sed -r "/^$c$/d"`
   for s in $subclasses; do
-    record=`echo getThe"$c"Super "$s"`
+    # record=`echo getThe"$c"Super "$s"`
     # echo $record
     # echo $record >> "$utildir/subclasses.txt"
     sub_path="$s.java"
