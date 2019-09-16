aa668e38ea913aa7552aaab74174bfce86dd4f22
Lakshamana <guitrompa1@gmail.com>
2019-09-15 22:41:10 -0300

    rename file IGroup to IWorkGroup

--- src/main/java/br/ufpa/labes/spm/repository/interfaces/agent/IWorkGroupDAO.java
+++ src/main/java/br/ufpa/labes/spm/repository/interfaces/agent/IWorkGroupDAO.java
@@ -4,6 +4,6 @@ package br.ufpa.labes.spm.repository.interfaces.agent;
 import br.ufpa.labes.spm.repository.interfaces.IBaseDAO;
 import br.ufpa.labes.spm.domain.WorkGroup;
 
-public interface IGroupDAO extends IBaseDAO<WorkGroup, String>{
+public interface IWorkGroupDAO extends IBaseDAO<WorkGroup, String>{
 
 }
