29ab9c365dfbaf49f2d5f4bcdbecd60ceead10e2
Lakshamana <guitrompa1@gmail.com>
2019-09-16 01:27:52 -0300

    add subclasses file

--- util_files/inheritance.sh
+++ util_files/inheritance.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+utildir="`readlink -m .`/util_files"
 cd $HOME/Documentos/spm_root/SPMServices/ejbModule/org/qrconsult/spm
 classes=`grep -iRl '@Inheritance(strategy=InheritanceType.JOINED)' | sed '/policies/d' | cut -d'/' -f3 | sed -r 's/(.*).java/\1/'`
+
+echo "current: $currentdir"
 
 cd $HOME/git/spm/
 model_path='src/main/java/br/ufpa/labes/spm/domain'
@@ -25,8 +28,11 @@ for c in $classes; do
   sed -ri "/package/ s/(package .*)/\1\n\nimport javax.persistence.Inheritance;\nimport javax.persistence.InheritanceType;\n/" $class_path
 
   # Add 'extends' to subclasses
-  subclasses=`grep -iRl getThe"$c"Super | sed -r 's/(.*).java/\1/'`
+  subclasses=`cat "$utildir/subclasses.txt" | grep "$c" | awk '{print $2}'`
   for s in $subclasses; do
+    record=`echo getThe"$c"Super "$s"`
+    # echo $record
+    # echo $record >> "$utildir/subclasses.txt"
     sub_path="$s.java"
     sed -ri "/public class/ s/public class $s (.*)/public class $s extends $c \1/" $sub_path
   done
