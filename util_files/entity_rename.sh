d8acc3a4674280d21927285d6764a05a9fd88507
Lakshamana <guitrompa1@gmail.com>
2019-09-15 22:40:34 -0300

    rename Join,Branch.* classes

--- util_files/entity_rename.sh
+++ util_files/entity_rename.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in `find . -regextype posix-extended -regex '.*[I]*[Req]*GroupDAO.java'`; do
+for file in `find . -regextype posix-extended -regex '.*(Join|Branch)(AND)*([^(Con)]*)(.*)DAO.java'`; do
   echo $file
-  replace=`echo $file | sed -r 's/(.*[I]*[Req]*)(GroupDAO.java)/\1Work\2/'`
+  replace=`echo $file | sed -r 's/(.*)(Join|Branch)(AND)*([^(Con)]*)(.*)DAO.java/\1\2\3Con\5DAO.java/'`
   mv $file $replace
 done
 
