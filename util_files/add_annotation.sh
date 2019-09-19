0d99fb3c938940f0b5405d8a0071d86ab9c30b24
Lakshamana <guitrompa1@gmail.com>
2019-09-18 22:50:26 -0300

    fix add_annotation

--- util_files/add_annotation.sh
+++ util_files/add_annotation.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-utildir="`readlink -m .`/util_files"
 cd src/main/java/br/ufpa/labes/spm/repository
 classes=`find . -regextype posix-extended -regex '(.*)Repository.java' | cut -d'/' -f2 | sed -r 's/(.*)Repository.java/\1/'`
 
 for c in $classes; do
-  class_name=`echo "$c" | cut -d'/' -f4 | sed -r 's/(.*).java/\1/'`
-
+  class_path=`echo "$c"Repository.java`
   # Add DAO inheritance
   super=`echo I"$c"DAO`
-  sed -ri "/public class/ s/public class $c (.*)/public class $c extends $super \1/" $class_path
+  package=`find interfaces/ -iname "$super".java | cut -d'/' -f2`
+
+  sed -ri "/package/ s/(package .*)/\1\n\nimport br.ufpa.labes.spm.repository.interfaces.$package.$super;\n/" $class_path
+  sed -ri "/public interface/ s/public interface "$c"Repository extends (.*)/public interface "$c"Repository extends $super, \1/" $class_path
 done
 
 echo 'Finish adding inheritance...'
